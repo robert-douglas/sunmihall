781ba50222038f0f26744aadfccbb3f6d331d150
John <jlross90@gmail.com>
2018-11-13 13:19:20 +0900

    Add lesson schedule, remove pricing tables, add contact form content

--- contact.php
+++ contact.php
@@ -46,13 +46,21 @@ if( empty($errors))
 // $to = $myemail;
 $to = "jlross90@gmail.com";
 
-$email_subject = "Contact form submission: $name";
+$email_subject = "ウェブサイトよりお問い合わせが届きました";
 
-$email_body = "You have received a new message. ".
+$email_body = "差出人: $name
 
-" Here are the details:\n Name: $name \n Phone: $phone \n ".
+メールアドレス: $email
 
-"Email: $email \n Please call during the following times: $time \n Message: \n $message";
+電話番号: $phone
+
+お問い合わせ内容: $message
+
+折り返しのお電話ご希望の時間帯: $time
+
+____________________________
+このメールは http://sunmihall.com/ のお問い合わせフォームから送信されました
+"
 
 $headers = "From: $myemail\n";
 
@@ -64,22 +72,52 @@ mail($to,$email_subject,$email_body,$headers);
 // AUTORESPONDER
 
 /* Autoresponder subject */
-$respond_subject = "Sun Mihall - Thank you for contacting us!";
+$respond_subject = "【サンミハール】お問い合わせありがとうございます（自動返信メール）";
 
 /* Autoresponder message */
-$respond_message = "Hello!
+$respond_message = "＜こちらはホームページより自動送信を行っております＞
 
-Thank you for contacting us! Your message has been recieved and we will get back to you
-as soon as possible!
+こんにちは。
+サンミハールランゲージサービスです。
 
-Yours sincerely,
+この度は、ウェブサイトよりお問い合わせいただき、
+誠にありがとうございます。
 
-Sun Mihall
-www.sunmihall.com
+後ほど担当者より改めてご連絡をさせていただきますので
+今しばらくお待ちくださいますようお願いいたします。
+
+尚、ご入力いただきました情報は、厳重に管理いたします。
+第三者への開示は一切致しませんのでご安心ください。
+
+--------------------------------------------------------
+お名前:$name 様
+
+メールアドレス: $email
+
+電話番号: $phone
+
+お問い合わせ内容: $message
+
+折り返しのお電話ご希望の時間帯: $time
+--------------------------------------------------------
+
+※このメールは自動返信です。内容にお心当たりがない
+場合は、大変お手数ですが下記までご連絡ください。
+
+
+＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+サンミハールランゲージサービス
+神戸市中央区三宮町1-4-13 YSビル4F
+Phone: 078-331-3035 （日祝休)
+Email：study@sunmihall.com
+URL: www.sunmihall.com
+マネージャー　フェンスターメイカー明実
+＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊
+
 ";
 
 /* Send the message using mail() function */
-mail($email, $respond_subject, $respond_message);
+mail($email, $respond_subject, $respond_message, $headers);
 
 //redirect to the 'thank you' page
 
